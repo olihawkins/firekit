85268283929c568bd9e5faad907df0b3dc3619bf
Oli Hawkins <oli@olihawkins.com>
2022-09-01 12:25:18 +0100

    Added read mode to ImageDataset

--- firekit/vision/_imagedataset.py
+++ firekit/vision/_imagedataset.py
@@ -17,12 +17,24 @@ class ImageDataset(Dataset):
     def __init__(
         self, 
         data, 
+        read_mode=None,
         transform=None, 
         target_transform=None):
 
         self.data = data
         self.transform = transform
         self.target_transform = target_transform
+
+        if read_mode == "GRAY":
+            self.read_mode = ImageReadMode.GRAY
+        elif read_mode == "GRAY_ALPHA":
+            self.read_mode = ImageReadMode.GRAY_ALPHA
+        elif read_mode == "RGB":
+            self.read_mode = ImageReadMode.RGB
+        elif read_mode == "RGB_ALPHA":
+            self.read_mode = ImageReadMode.RGB_ALPHA
+        else:
+            self.read_mode = ImageReadMode.UNCHANGED
 
     def __len__(self):
         return len(self.data)
@@ -31,7 +43,7 @@ class ImageDataset(Dataset):
         
         image_path = self.data.iloc[idx, 0]
         image_label = self.data.iloc[idx, 1]
-        image = read_image(image_path, ImageReadMode.RGB).type(torch.float32)
+        image = read_image(image_path, self.read_mode).type(torch.float32)
         label = torch.tensor(image_label, dtype=torch.float32).unsqueeze(0)
         
         if self.transform:
